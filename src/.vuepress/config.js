281b8462af9d685ab23fcd2c8a5248251358681f
Meena <30926214+Meena111@users.noreply.github.com>
2021-03-12 18:27:07 +0530

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'Vuepress Docs Boilerplate',
+  title: 'Shoppre Commerce Docs',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
